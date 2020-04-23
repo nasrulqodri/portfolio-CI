3c843dc26e3275f849ad2fe2547d0c8ea1bcde74
arul-cloud <arul.qodrie@gmail.com>
2020-04-23 10:58:54 +0700

    css

---
+++ application/views/inc/custom_css.php
@@ -0,0 +1,83 @@
+<style type="text/css">
+	body{
+		background-color: #126e66;
+	}
+	#welcome{
+		height: 100vh;
+	}
+	.welcome-text{
+		position: relative; 
+		top: 35%;
+	}
+	#container-profile, #map{
+		display: none;
+	}
+	#container-profile{
+		height: 660px;
+	}
+	.profile-box{
+		padding: 7%;
+	}
+	.profile-box-left{
+		/*height: 500px;*/
+		background-color: #14655c;
+	}
+	.content{
+		height: 100%;
+		display: inline-grid;
+		padding: 100px 0;
+	}
+	.img-round{
+		border-radius: 50%;
+	}
+	.gender{
+		color: aquamarine;
+	}
+	.profile-box-right{
+		/*height: 500px;*/
+		background-color: #175f56;
+	}
+	#container-profile .tab-menu{
+		display: flex;
+		justify-content: center;
+	}
+	#container-profile .nav a{
+		color: darkseagreen;
+	}
+	.content-list{
+		padding: 0 15%;
+	}
+	.nav li.active a, .nav li.active a:focus{
+		background-color: #00bb79;
+	}
+	.nav li.active a{
+		color: #fff !important;
+	}
+	.nav li:hover a{
+		color: #175f56 !important;
+	}
+	.title1{
+		color: aquamarine; 
+		margin: 0;
+	}
+	.title2{
+		padding-bottom: 10px;
+		border-bottom: 1px solid #fff;
+	}
+	.next-profile{
+		position: absolute; 
+		bottom: 30px; 
+		right: 50px;
+	}
+	footer{
+		position: absolute;
+		z-index: 1;
+		width: 100%;
+		/*bottom: 0;*/
+		background-color: #000;
+	}
+	footer p{
+		margin: 0; 
+		padding: 15px;
+	}
+</style>
